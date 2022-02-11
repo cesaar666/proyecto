df5907ab6d976f55c7edd08568cbc9975625952b
cesaar666 <cesarrobles20166@gmail.com>
2022-02-11 15:36:23 -0600

    primer formulario

---
+++ resive_get.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+Nombre: <?php echo $_GET['Nombre'] ?> <br>
+ usuario: <?php echo $_Get['Usuario'] ?> <br>
+ Contrasena: <?php echo $_GET['Usuario'] ?> <br>
+</body>
+</html>
---
+++ resive_post.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datos</title>
+</head>
+<body>
+
+ Nombre: <?php echo $_POST['Nombre'] ?> <br>
+ usuario: <?php echo $_POST['Usuario'] ?> <br>
+ Contrasena: <?php echo $_POST['Usuario'] ?> <br>
+
+</body>
+</html>
