52c0da5e1e1adee5bf1364c8ab396de145d1e597
cesaar666 <cesarrobles20166@gmail.com>
2022-02-12 07:46:44 -0600

    nuevos archivos

---
+++ aritmeticos.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>resive valores</title>
+</head>
+<body>
+    <?php
+    $valor1 = $_POST['valor1'];
+    $valor2 = $_POST['valor2'];
+    ?>
+</body>
+</html>
